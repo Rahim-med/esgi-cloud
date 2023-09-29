478c7a3de0428f3223e7e12cd58b323ea87b4422
rahim <ameddas@myges.com>
2023-09-29 12:30:57 +0200

    first commit

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app'
 
 const app = initializeApp({
-  apiKey: "", // TODO
+  apiKey: "AIzaSyDtWtN5EEVCUM__z7hu5rhO0JAaa6uNrc8", // TODO
   authDomain: "esgi-cloud-390e3.firebaseapp.com", // TODO
   databaseURL: "", // TODO
   projectId: "esgi-cloud-390e3", // TODO
